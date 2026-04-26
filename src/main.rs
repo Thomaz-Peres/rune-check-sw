3efaf0ae61efaa964b8d990e92b0547240e758d6
Thomaz Peres <thomaz.peres21@hotmail.com>
2026-04-26 18:20:01 -0300

    Changing everything

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use rcgen::{BasicConstraints, CertificateParams, DnType, IsCa, Issuer, KeyPair};
-use rustls::crypto::aws_lc_rs;
-use std::{fs, net::SocketAddr};
-
-use hudsucker::{
-    Body, HttpContext, HttpHandler, Proxy, RequestOrResponse,
-    certificate_authority::RcgenAuthority,
-    hyper::{Request, Response},
+use std::fs;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
 };
 
-fn generate_root_ca() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Generating new Root CA...");
+use std::io::{Read, Write};
 
+use rcgen::{CertificateParams, IsCa, KeyPair};
+
+#[tokio::main]
+async fn main() {
+    if fs::exists("ca.key").is_err() && fs::exists("ca.crt").is_err() {
+        if let Err(e) = generate_root_ca() {
+            eprintln!("Failed to generate CA: {}", e);
+        }
+    }
+
+    listener().await;
+    println!("Hello world");
+}
+
+fn generate_root_ca() -> Result<(), Box<dyn std::error::Error>> {
     let mut params = CertificateParams::default();
-    params.is_ca = IsCa::Ca(BasicConstraints::Unconstrained);
+
+    params.is_ca = IsCa::Ca(rcgen::BasicConstraints::Unconstrained);
+
     params
         .distinguished_name
-        .push(DnType::CommonName, "Summoners War Rust Proxy CA");
+        .push(rcgen::DnType::CommonName, "Summoners War Rust Proxy CA");
 
     let key_pair = KeyPair::generate()?;
 
@@ -27,77 +39,42 @@ fn generate_root_ca() -> Result<(), Box<dyn std::error::Error>> {
     fs::write("ca.crt", cert_pem)?;
     fs::write("ca.key", private_key_pem)?;
 
-    println!("Success! Saved ca.crt and ca.key to the project root.");
+    println!("Success! Certificate and key are saved to the project root.");
 
     Ok(())
 }
 
-#[tokio::main]
-async fn main() {
-    if fs::exists("ca.key").is_err() {
-        if let Err(e) = generate_root_ca() {
-            eprintln!("Failed to generate CA: {}", e);
-        }
-    }
-
-    if let Err(e) = proxy().await {
-        eprintln!("Failed to create proxy: {}", e);
-    }
-}
-
-async fn proxy() -> Result<(), Box<dyn std::error::Error>> {
-    let private_key = fs::read_to_string("ca.key").expect("Failed to read ca.key");
-    let ca_cert = fs::read_to_string("ca.crt").expect("Failed to read ca.crt");
-
-    let key_pair = KeyPair::from_pem(&private_key).expect("Failed to parse private key");
-
-    let issuer = Issuer::from_ca_cert_pem(&ca_cert, key_pair).expect("Failed to ");
-
-    let ca = RcgenAuthority::new(issuer, 1_100, aws_lc_rs::default_provider());
-
-    //ProxyBui
-    let proxy = Proxy::builder()
-        .with_addr(SocketAddr::from(([127, 0, 0, 1], 8080)))
-        .with_ca(ca)
-        .with_rustls_connector(aws_lc_rs::default_provider())
-        .with_http_handler(SwProxy)
-        .build()?;
-
-    println!("Proxy listening on 127.0.0.1:8080. Ready to intercept!");
-
-    tokio::spawn(proxy.start());
-
-    tokio::signal::ctrl_c()
+async fn listener() {
+    let listener = TcpListener::bind("127.0.0.1:443")
         .await
-        .expect("failed to listen for Ctrl+c");
+        .expect("Could not bind to address");
 
-    println!("Shutting down proxy");
+    println!("Listening on port 443");
 
-    Ok(())
-}
+    loop {
+        // Accept a new connection
+        let (mut socket, addr) = listener.accept().await.expect("Fail lo load");
+        println!("New client connected: {}", addr);
 
-#[derive(Clone)]
-struct SwProxy;
+        // Spawn a task to handle the connection concurrently
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => return, // Connection closed
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("Failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
 
-impl HttpHandler for SwProxy {
-
-    async fn handle_request(&mut self, _ctx: &HttpContext,
-        req: Request<Body>,
-    ) -> RequestOrResponse {
-        println!("URL: {}", req.uri());
-
-        println!("Body: {:?}", req.body());
-
-        RequestOrResponse::Request(req)
-    }
-
-    async fn handle_response(&mut self, _ctx: &HttpContext,
-        response: Response<Body>,
-    ) -> Response<Body> {
-        println!("Intercepted a response with status: {}", response.status());
-
-        // _ctx.client_addr
-
-        response
+                // Echo the data back to the client
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("Failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
     }
 }
