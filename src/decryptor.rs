b94738511add92630b3d6bb65b45cf20a3f91163
Thomaz Peres <thomaz.peres21@hotmail.com>
2026-07-08 00:05:51 -0300

    Forgot to upload decryptor

--- src/decryptor.rs
+++ src/decryptor.rs
@@ -1,29 +1,28 @@
 use aes::cipher::{BlockModeDecrypt, KeyIvInit, block_padding::Pkcs7};
-use base64::Engine;
-use flate2::read::ZlibDecoder;
-use std::io::Read;
+use base64::{Engine, engine::general_purpose::STANDARD};
+use flate2::{read::ZlibDecoder};
+use std::io::{Read};
 
 type Aes128CbcDec = cbc::Decryptor<aes::Aes128>;
 
-fn decrypt_to_bytes(b64: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let sw_iv: [u8; 16] = [0u8; 16];
-    let sw_key: [u8; 16] = [
+const SW_KEY: [u8; 16] = [
         71, 114, 52, 83, 50, 101, 105, 78, 108, 55, 122, 113, 53, 77, 114, 85,
     ];
+const SW_IV: [u8; 16] = [0u8; 16];
 
-    let ciphertext = base64::engine::general_purpose::STANDARD.decode(b64)?;
-    let plaintext = Aes128CbcDec::new(&sw_key.into(), &sw_iv.into())
+fn decrypt_to_bytes(b64: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let ciphertext = STANDARD.decode(b64.trim_ascii())?;
+    let plaintext = Aes128CbcDec::new(&SW_KEY.into(), &SW_IV.into())
         .decrypt_padded_vec::<Pkcs7>(&ciphertext)
         .map_err(|e| format!("AES decrypt/unpad failed: {:?}", e))?;
     Ok(plaintext)
 }
-
-pub fn decrypt_request(b64: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let bytes = decrypt_to_bytes(b64.trim())?;
+pub fn decrypt_request(b64: &[u8]) -> Result<String, Box<dyn std::error::Error>> {
+    let bytes = decrypt_to_bytes(b64)?;
     Ok(String::from_utf8(bytes)?)
 }
 
-pub fn decrypt_response(b64: &str) -> Result<String, Box<dyn std::error::Error>> {
+pub fn decrypt_response(b64: &[u8]) -> Result<String, Box<dyn std::error::Error>> {
     let compressed = decrypt_to_bytes(b64)?;
     let mut decoder = ZlibDecoder::new(&compressed[..]);
     let mut json = String::new();
@@ -35,33 +34,34 @@ pub fn decrypt_response(b64: &str) -> Result<String, Box<dyn std::error::Error>>
 mod tests {
     use super::*;
 
+    const SAMPLE_REQUEST: &str = r#"{"command":"Ping","wizard_id":1,"hello":"world"}"#;
+    const SAMPLE_RESPONSE: &str = r#"{"command":"Ping","ret_code":0,"payload":[1,2,3]}"#;
+
+    const REQUEST_BASE64: &str = "p+is24btlT/udsBYTcNdYRbUkiGHQE2lZ6QOhvQdsjqQK1ZFLG1St1quOLAmWnS793B+nz0GHCJikIsH2eqsUA==";
+    const RESPONSE_BASE64: &str = "fV9W7uRzfb8Ly3OOcIrGX7oMh34scfnjHj8sNyQexsC1j0gNSp4ViZ4rIgwLsb1AyVsoUCyn5TPj3RI5MGG8IA==";
+
+
     #[test]
     fn request() {
-        let req_b64 = "vtgSQBCkzpT+duvn5CVFKBy943E8fjhK8k/mI0AjYkifsgqqIangMvjQK5ExCvz5NQyB9/rsvQAFjMhAHFzD14liV0jxTXzbfnIPQb0RbgXUsbeioa662Wv5aesggfbfqLqm8QAuMCTcvSZdlsHx/xwWGqS8ZCmt2oKlRz4iJEqu32cTtsZ8ekkEUcHLm89G4ncbdaC0ZSH2nP0QSYBsB4abIQn8+dpLsEiX2CmtlCTP7YqsFl1KJ4+x/du6AmZ1ZIP0i6yNaZwkxIGUlh7uOZi4fIrpGjhzU8Xg0SuOqmRvyuPHF30/8ULY4RjVaRzrW7fgcVmcN0OBzRWws9rggEFo0YHaQN3uEVBhAbxVeByzesyrr5WCJfvIEOJQKR04CYc2gJWyzlyr/L2nCS5RLm9y9jTSAPqGbPwa4UMjHGyPcJkkOCnNyjAKb8/acFd1PkbQ7Cx9756pt0GBUFNRsg==";
-
-        println!("=== test_decryption_against_capture ===");
-
-        let x = decrypt_request(req_b64)
+        let x = decrypt_request(REQUEST_BASE64.as_bytes())
             .inspect(|json| println!("REQUEST JSON:\n{}\n", json))
             .inspect_err(|e| eprintln!("REQUEST FAILED: {}\n", e));
 
         println!("=======================================");
 
         assert!(x.is_ok());
+        assert_eq!(x.unwrap(), SAMPLE_REQUEST);
     }
 
     #[test]
     fn response() {
-        let req_b64 = "33tZirRCpp4WoYEe0wWTtAdm89YtvRXoVpUz3Ti/FsukjnQlY6Z5n4bB610RD239izC0/lLpTdKLCVWHEnnKLL0nz0PRJglwwiQtTApIWHbPEjRkmNalywSdMNCtYd2X4L4ZBYw1vWfIkzOnRcGTSf0tKvD6YzMuneEoS6rrJEtjJtfnxRBzrgHb1V+4S0c/Eas4fsTnzbpt9FdlN9vRij/mjeMdxy8LoIgJvYBuE+LgRLskpdm1adV3BwDYUj06vaPY6siJX4989CVefwoDfX0FVgsZVJT/ARwZY1CpW82zjnfHeBsLyHg1NLx05sUpCwXNCXLGy338gfXBjvtc71CBgSJsV05eg2ncRa5UdJ0PlmSmRXN0WnAaIUklRMavk3ZyYID0pIWYNxVPYNdK43VAtrZetTdzKBLv/ArPw9QCDkVYGqMPfvQg/dMUvOlaq6qYmU3OLYjdIbLq9Hkpc5LjOCjrqAROFQif2+xMhn4TpOPS75Q6Khl2L0AnK7vCIpw+60KeYXKx0eQLyfZdJfxHiAOLUFWIUiS4U+vlGIMNhvx3e0xV9J6fDlQX5BNBzPWIevVCol8ZQ4+feL50e51D+siA+yAVa6Gukx64Pr0IBaGRi48EvT4Y9M7kzMLLNL0oKGi5pr4Lt8Aw0ngsjX9Gr+CL/5LxAU9R7RXAfO0dHK9TV/Q97YcUe7Vu1DvAcFvhXSVgNsGUk8itCy6lS+BkQu5WWr6btZ8tzpHql4zuKBz4fEgwKfO6VvMiMnO+YduVWa0AmUqD6rT6LpdOatUGY3H1ep9MyW41Pex0l2Z0SLL+SBngcywyjmK8gSPBJ2dAccX6ErfKpq14q6aiMZX6+g74NQV18+jSCfht6c7QlKVfUkW7qvGpHhDW3tyPiOGYoXbFCGsIP/O2rV6IJhuL9l2lI8MIgWFH0N8l35iy/omjQSKZSemV6U5InzZ2xk5VO6QFOEGWKLhdCSHk2Yp8ZHR+EcI1BqjQXHWkVe8=";
-
-        println!("=== test_decryption_against_capture ===");
-
-        let x = decrypt_response(req_b64)
+        let x = decrypt_response(RESPONSE_BASE64.as_bytes())
             .inspect(|json| println!("REQUEST JSON:\n{}\n", json))
             .inspect_err(|e| eprintln!("REQUEST FAILED: {}\n", e));
 
         println!("=======================================");
 
         assert!(x.is_ok());
+        assert_eq!(x.unwrap(), SAMPLE_RESPONSE);
     }
 }
